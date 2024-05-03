4dc09052b9861189ae2390c90806ca7a792902a8
Fabian Tamp <fabian.tamp@gmail.com>
2024-05-03 11:29:28 +0200

    Reformat a bunch of stuff

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 we all need more reasons to celebrate.
 
 ```sh
-caddy file-server --listen :8080 --root .
+npm install
+npm run dev | build | preview
 ```
-
-## deps
-
-- `van-1.5.0.min.js` in the source tree is https://github.com/vanjs-org/van
--- src/scriptz.ts
+++ src/scriptz.ts
@@ -1,102 +1,101 @@
 import van from "vanjs-core"
 
-const { a, b, div, h1, h2, table, thead, tbody, input, tr, th, td, li, p, ul } = van.tags;
+const { b, div, h2, table, thead, tbody, input, tr, th, td, p } = van.tags;
 
 const MILLIS_TO_DAYS = 1 / (1000 * 60 * 60 * 24);
 
 const LIST_OF_SIGNIFICANT_DAYCOUNTS = (() => {
-    const milestones = [];
-    // not so many people live to 121 years == ~44k days
-    for (var i = 1; i < 44; i++) {
-        milestones.push(i * 1000);
-    }
-    milestones.push(
-        1234,
-        12345,
-        3141,
-        31415,
-        1111,
-        2222,
-        3333,
-        4444,
-        5555,
-        6666,
-        7777,
-        8888,
-        9999,
-        11111,
-        22222,
-        33333,
-        44444,
-    )
-    milestones.sort((a, b) => a - b);
-    return milestones;
+  const milestones = [];
+  // not so many people live to 121 years == ~44k days
+  for (var i = 1; i < 44; i++) {
+    milestones.push(i * 1000);
+  }
+  milestones.push(
+    1234,
+    12345,
+    3141,
+    31415,
+    1111,
+    2222,
+    3333,
+    4444,
+    5555,
+    6666,
+    7777,
+    8888,
+    9999,
+    11111,
+    22222,
+    33333,
+    44444,
+  )
+  milestones.sort((a, b) => a - b);
+  return milestones;
 })();
 
-const Table = ({ head, data }) => table(
-    head ? thead(tr(head.map(h => th(h)))) : [],
-    tbody(data.map(row => tr(
-        row.map(col => td(col)),
-    ))),
+const Table = ({ head, data }: { head: (HTMLElement | string)[], data: (HTMLElement | string)[][] }) => table(
+  head ? thead(tr(head.map(h => th(h)))) : [],
+  tbody(data.map(row => tr(
+    row.map(col => td(col)),
+  ))),
 );
 
-const addDays = (date, days) => {
-    const d = new Date(date.valueOf());
-    d.setDate(date.getDate() + days);
-    return d;
+const addDays = (date: Date, days: number) => {
+  const d = new Date(date.valueOf());
+  d.setDate(date.getDate() + days);
+  return d;
 }
 
 // returns (int, Date)[]
-const computeDays = (startDate) => {
-    console.log(startDate);
-    if (!startDate) {
-        return [[]];
-    }
+const computeDays = (startDate: Date): [number, Date][] => {
+  console.log(startDate);
+  if (!startDate) {
+    return [];
+  }
 
-    const dayCutoff = (new Date() - startDate) * MILLIS_TO_DAYS;
-    var relevantStartIdx = 0;
-    for (var i = 0; i < LIST_OF_SIGNIFICANT_DAYCOUNTS.length; i++) {
-        if (LIST_OF_SIGNIFICANT_DAYCOUNTS[i] >= dayCutoff) {
-            relevantStartIdx = i;
-            break;
-        }
+  const dayCutoff = (new Date().getTime() - startDate.getTime()) * MILLIS_TO_DAYS;
+  var relevantStartIdx = 0;
+  for (var i = 0; i < LIST_OF_SIGNIFICANT_DAYCOUNTS.length; i++) {
+    if (LIST_OF_SIGNIFICANT_DAYCOUNTS[i] >= dayCutoff) {
+      relevantStartIdx = i;
+      break;
     }
-    if (relevantStartIdx > LIST_OF_SIGNIFICANT_DAYCOUNTS.length) {
-        // don't blow past end of the array
-        // TODO add better handling for people messing with really really old days
-        relevantStartIdx = 0;
-    }
+  }
+  if (relevantStartIdx > LIST_OF_SIGNIFICANT_DAYCOUNTS.length) {
+    // don't blow past end of the array
+    // TODO add better handling for people messing with really really old days
+    relevantStartIdx = 0;
+  }
 
-    return LIST_OF_SIGNIFICANT_DAYCOUNTS.slice(relevantStartIdx).map((days) => [days, addDays(startDate, days)]);
+  return LIST_OF_SIGNIFICANT_DAYCOUNTS.slice(relevantStartIdx).map((days) => [days, addDays(startDate, days)]);
 };
 
 const MiniApp = () => {
-    const birthday = van.state();
-    const shouldDisplay = van.derive(() => !!birthday.val);
-    const daysAgo = van.derive(() => Math.floor((new Date() - new Date(birthday.val)) * MILLIS_TO_DAYS));
-    const allDates = van.derive(() => computeDays(new Date(birthday.val)));
-    console.log(birthday.value);
-    return div(
-        h2("When's your birthday?"),
-        p("Please include the year. Don't worry, we won't tell anyone."),
-        input({
-            type: "date",
-            value: birthday,
-            oninput: e => {
-                birthday.val = e.target.value;
-            }
-        }),
-        () => shouldDisplay.val ?
-            div(
-                p(
-                    "Nice! You were born ",
-                    b(daysAgo),
-                    " days ago today!",
-                ),
-                p("Maybe you'd like to celebrate these future milestones:"),
-                Table({ head: ["Occasion", "Date"], data: allDates.val.map(([day, date]) => [day, date.toLocaleDateString()]) })
-            ) : "",
-    );
+  const birthday = van.state<string>();
+  const shouldDisplay = van.derive(() => !!birthday.val);
+  const daysAgo = van.derive(() => Math.floor((new Date().getTime() - new Date(birthday.val).getTime()) * MILLIS_TO_DAYS));
+  const allDates = van.derive(() => computeDays(new Date(birthday.val)));
+  return div(
+    h2("When's your birthday?"),
+    p("Please include the year. Don't worry, we won't tell anyone."),
+    input({
+      type: "date",
+      value: birthday,
+      oninput: e => {
+        birthday.val = e.target.value;
+      }
+    }),
+    () => shouldDisplay.val ?
+      div(
+        p(
+          "Nice! You were born ",
+          b(daysAgo),
+          " days ago today!",
+        ),
+        p("Maybe you'd like to celebrate these future milestones:"),
+        Table({ head: ["Occasion", "Date"], data: allDates.val.map(([day, date]) => [day.toLocaleString(), date.toLocaleDateString()]) })
+      ) : "",
+  );
 }
 
 van.add(document.body, MiniApp());
